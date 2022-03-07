4ab8a3ea113502a37758775dc50472602723ca29
Doreen <doreen7932@gmail.com>
2022-03-07 17:02:55 +0800

    MainActivity.kt lambda and listener

--- app/src/main/java/com/example/bmi2/MainActivity.kt
+++ app/src/main/java/com/example/bmi2/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.bmi2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.bmi2.databinding.ActivityMainBinding
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,12 +16,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//        binding.bHelp.setOnClickListener {
+//            Log.d("MainActivity", "Need help!")
+//        }
+        binding.bHelp.setOnClickListener {
+            Log.d("MainActivity", "help clicked")
+        }
     }
 
     fun bmi(view: View) {
+//        Log.v() //verble???
+        Log.d("MainActivity", "clicked") //debug after online delete
+//        Log.i() //information after online sometimes delete
+//        Log.w() //warining
+//        Log.e() //error
         println("hahaha")
-        var Weight = binding.edWeight.text.toString().toFloat()
-        var Height = binding.edHeight.text.toString().toFloat()
-        println(Weight/(Height*Height))
+        var weight = binding.edWeight.text.toString().toFloat()
+        var height = binding.edHeight.text.toString().toFloat()
+        var bmi = weight/(height*height)
+        Log.d("MainActivity", bmi.toString())
+        Toast.makeText(this, "Your BMI $bmi", Toast.LENGTH_LONG)
+//        println(weight/(height*height))
+//        val builder = AlertDialog.Builder(this)
+//        builder.setTitle("Hello")
+//        builder.setMessage("Your BMI is $bmi")
+//        builder.setPositiveButton("OK", null)
+//        val dialog = builder.create()
+//        dialog.show()
+        AlertDialog.Builder(this)
+            .setTitle("Hello")
+            .setMessage("Your BMI is $bmi")
+            .setPositiveButton("OK",null)
+//            .show()
+        binding.tvBmi.text = "Your BMI is ${bmi.roundToInt()}"
     }
 }
